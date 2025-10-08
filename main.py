10444940500ec5cc69648cab23c512982121ea66
Ismail Baig M <ismailece2007@gmail.com>
2025-10-08 14:12:26 +0530

    .venv added, motor added to get data from Mongo
    .venv added, motor added to get data from Mongo

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from fastapi import FastAPI
+from bson import ObjectId
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from motor.motor_asyncio import AsyncIOMotorClient
+
 
 app = FastAPI()
+
+
 
 # If you want to allow *all* origins (i.e. fully global)
 origins = ["*"]
@@ -22,7 +27,45 @@ app.add_middleware(
     # optionally: expose_headers, allow_origin_regex, max_age, etc.
 )
 
+
+# MongoDB connection
+client = AsyncIOMotorClient("mongodb://localhost:27017")
+db = client.mschooldb
+collection = db.studentdetails
+
+def serialize_doc(doc):
+    doc["_id"] = str(doc["_id"])
+    return doc
+
 # Then define your endpoints
-@app.get("/my-endpoint")
+@app.get("/")
 async def some_endpoint():
     return {"msg": "Hello from FastAPI!"}
+
+@app.get("/getuserdetails")
+async def get_user_details():
+    users_cursor = collection.find()
+    users = await users_cursor.to_list(length=100)
+    return [serialize_doc(user) for user in users]
+
+@app.get("/users/{user_id}")
+async def get_user(user_id: int):
+    user = await collection.find_one({"user_id": user_id})
+    if not user:
+        return {"error": "User not found"}
+    return serialize_doc(user)
+
+
+# @app.get("/users/update/{user_id}")
+# async def update_user(user_id: int):
+#     """Update an existing user (replace fields given)."""
+#     #  update_data = {k: v for k, v in user_data.dict().items() if v is not None}
+#     result = await collection.update_one({"user_id": user_id}, 
+#                                          {"$set": {"name": "Shaik Sameer 1"}})
+
+#     if result.matched_count == 0:
+#         raise HTTPException(status_code=404, detail="User not found")
+
+#     updated_user = await collection.find_one({"user_id": user_id})
+#     return serialize_doc(updated_user)
+
